bd4affd8dd79d64551678bc300a8e2ed24ec1763
aticie <efehanatici@gmail.com>
2020-04-14 11:10:43 +0300

    update player roles by registered teams

--- main.py
+++ main.py
@@ -2,8 +2,7 @@ import os
 import discord
 from discord.ext import commands
 
-from database import read_tournament_db, write_tournament_db, get_settings
-from registration_check import get_players_by_discord
+from database import read_tournament_db, get_settings, read_qualifier_results_db
 
 settings = get_settings()
 
@@ -17,34 +16,29 @@ for cog in cog_list:
 
 @client.event
 async def on_ready():
-    db = read_tournament_db()
+    teams_db = read_tournament_db()
+    qf_results = read_qualifier_results_db()["final_result"]["final_scores"][:16]
     guild = client.get_guild(402213530599948299)
     player_role = discord.utils.get(guild.roles, id=693574523324203009)
 
-    print(f"Checking player roles..")
+    qualified_players = []
+    for team in qf_results:
+        for _team in teams_db:
+            if _team["name"] == team["team_name"]:
+                qualified_players.append(_team["user1"])
+                qualified_players.append(_team["user2"])
+
     for member in guild.members:
-        id = member.id
         if player_role in member.roles:
-            player_found = False
-            for team in db["teams"]:
-                user1 = team["user1"]
-                user2 = team["user2"]
-                if (id == user1 or id == user2):
-                    player_found = True
-
-            if not player_found:
-                print(f"Removing {player_role} role from {member}")
+            if member.id not in qualified_players:
+                print(f"Removing role from: {member}")
                 await member.remove_roles(player_role)
+        else:
+            if member.id in qualified_players:
+                print(f"Removing role from: {member}")
+                await member.add_roles(player_role)
 
-        elif player_role not in member.roles:
-            for team in db["teams"]:
-                user1 = team["user1"]
-                user2 = team["user2"]
-                if (id == user1 or id == user2):
-                    print(f"Adding {player_role} role to {member}")
-                    await member.add_roles(player_role)
-
-    print(f"Bot Started!!")
+    print(f"Bot started!")
     return
 
 
